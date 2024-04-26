e40089aea5314df81461a7750aa4d6991091ad01
FikiArriza <160560471+FikiArriza@users.noreply.github.com>
2024-04-26 15:59:42 +0700

    update map

--- resources/views/admin/map/map.blade.php
+++ resources/views/admin/map/map.blade.php
@@ -1,13 +1,18 @@
 @extends('admin.main')
 @section('content')
 <div class="content">
-      <div class="col-md-7 mb-2">
+      <div class="col-mb-2">
         <div class="card h-100 ">
           <div class="card-header ">
             Google Maps
           <div class="card-body ">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63232.53324042809!2d112.5770342038662!3d-7.891582159568065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6299690e0506d%3A0x28a471f6abf0e7d0!2sAraya%20XXI!5e0!3m2!1sid!2sid!4v1713495118616!5m2!1sid!2sid" width="610" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            <div id="map" class="map"></div>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63232.53324042809!2d112.5770342038662!3d-7.891582159568065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6299690e0506d%3A0x28a471f6abf0e7d0!2sAraya%20XXI!5e0!3m2!1sid!2sid!4v1713495118616!5m2!1sid!2sid" width="970" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div id="map" class="map">Plaza Araya XXI
+              <br>
+              Alamat: Jl. Blimbing Indah Megah No.2, Purwodadi, Kec. Blimbing, Kota Malang, Jawa Timur 65126 <br>
+              Provinsi: Jawa Timur <br>
+              Telepon: (0341) 409310
+            </div>
           </div>
         </div>
       </div>
