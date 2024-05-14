d4ab68529f7a7c24d01418e0da59b18ea625d451
FikiArriza <160560471+FikiArriza@users.noreply.github.com>
2024-05-14 11:41:15 +0700

    bayar

--- resources/views/user/order.blade.php
+++ resources/views/user/order.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- Modal Pembayaran 1 -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style="color: black">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -40,52 +41,116 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kembali</button>
-                <button type="button" class="btn btn-primary disabled" id="submitButton" data-bs-toggle="modal">Lanjut</button>
+                <button type="button" class="btn btn-primary disabled" id="submitButton" data-bs-toggle="modal" data-bs-target="#pembayaran">Lanjut</button>
                 <button type="button" class="btn btn-primary d-none disabled" id="saveButton">Simpan</button>
             </div>
             
-            <script>
-                document.addEventListener("DOMContentLoaded", function() {
-                    const form = document.getElementById("submitForm");
-                    const namaInput = document.getElementById("nama");
-                    const noHpInput = document.getElementById("noHp");
-                    const switchButton = document.getElementById("flexSwitchCheckDefault");
-                    const lanjutButton = document.getElementById("submitButton");
-                    const simpanButton = document.getElementById("saveButton");
-
-                    function checkFormCompletion() {
-                        const isFormComplete = namaInput.value.trim() !== "" && noHpInput.value.trim() !== "";
-                        const isSwitchChecked = switchButton.checked;
-                        const isReadyToSubmit = isFormComplete && (isSwitchChecked || !isSwitchChecked);
-
-                        if (isReadyToSubmit) {
-                            lanjutButton.classList.remove("disabled");
-                            simpanButton.classList.remove("disabled");
-                        } else {
-                            lanjutButton.classList.add("disabled");
-                            simpanButton.classList.add("disabled");
-                        }
-                    }
-
-                    form.addEventListener("input", checkFormCompletion);
-                    switchButton.addEventListener("change", function() {
-                        if (this.checked) {
-                            lanjutButton.classList.add("d-none");
-                            simpanButton.classList.remove("d-none");
-                        } else {
-                            lanjutButton.classList.remove("d-none");
-                            simpanButton.classList.add("d-none");
-                        }
-                        checkFormCompletion();
-                    });
-
-                    checkFormCompletion();
-                });
-            </script>            
       </div>
     </div>
   </div>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const form = document.getElementById("submitForm");
+        const namaInput = document.getElementById("nama");
+        const noHpInput = document.getElementById("noHp");
+        const switchButton = document.getElementById("flexSwitchCheckDefault");
+        const lanjutButton = document.getElementById("submitButton");
+        const simpanButton = document.getElementById("saveButton");
 
+        function checkFormCompletion() {
+            const isFormComplete = namaInput.value.trim() !== "" && noHpInput.value.trim() !== "";
+            const isSwitchChecked = switchButton.checked;
+            const isReadyToSubmit = isFormComplete && (isSwitchChecked || !isSwitchChecked);
+
+            if (isReadyToSubmit) {
+                lanjutButton.classList.remove("disabled");
+                simpanButton.classList.remove("disabled");
+            } else {
+                lanjutButton.classList.add("disabled");
+                simpanButton.classList.add("disabled");
+            }
+        }
+
+        form.addEventListener("input", checkFormCompletion);
+        switchButton.addEventListener("change", function() {
+            if (this.checked) {
+                lanjutButton.classList.add("d-none");
+                simpanButton.classList.remove("d-none");
+            } else {
+                lanjutButton.classList.remove("d-none");
+                simpanButton.classList.add("d-none");
+            }
+            checkFormCompletion();
+        });
+
+        checkFormCompletion();
+    });
+</script>            
+<!-- End -->
+
+<!-- Modal Pembayaran 2 -->
+<div class="modal fade" id="pembayaran" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style="color: black">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Metode Pembayaran</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <form id="submitForm">
+                <p style="font-weight: bold; text-decoration : underline; padding-bottom: 10px;">Pilih Metode Pembayaran</p>
+                <div class="form-group">
+                    <!-- Bank -->
+                    <p style="font-weight:bold">BANK</p>
+                    <table>
+                        <tr>
+                            <td style="padding: 10px">
+                                <input class="form-check-input" type="checkbox" id="radioBRI" name="metodePembayaran" value="BRI" onchange="toggleInput()">
+                                <label for="radioBRI">
+                                    <img src="{{ asset('assets/img/bri.jpg') }}" alt="" height="70px" width="90px">
+                                </label>
+                            </td>
+                            <td style="padding: 10px">
+                                <input class="form-check-input" type="checkbox" id="radioBTN" name="metodePembayaran" value="BTN" onchange="togleInput()">
+                                <label for="radioBTN">
+                                    <img src="{{ asset('assets/img/b.png') }}" alt="" height="70px" width="90px">
+                                </label>
+                            </td>
+                        </tr>
+                    </table>
+                    <!-- ewallet -->
+                    <p style="font-weight:bold">E-WALLET</p>
+                    <table>
+                        <tr>
+                            <td style="padding: 10px">
+                                <input class="form-check-input" type="checkbox" id="gopay" name="metodePembayaran" value="gopay" onchange="toggelInput()">
+                                <label for="gopay">
+                                    <img src="{{ asset('assets/img/gopay.png') }}" alt="" height="70px" width="70px">
+                                </label>
+                            </td>
+                        </tr>
+                    </table>
+                    <!-- inputbank -->
+                    <div id="inputBank" style="display: none;">
+                        <label for="" style="margin-top: 10px; margin-bottom: 10px">Masukkan Nomor Rekening Bank</label>
+                        <input type="number" class="form-control" id="nama" name="nama" placeholder="Masukkan Nomor">
+                    </div>
+                    <!-- inputewallet -->
+                    <div id="inputEwallet" style="display: none;">
+                        <label for="" style="margin-top: 10px; margin-bottom: 10px">Masukkan Nomor HP</label>
+                        <input type="number" class="form-control" id="nama" name="nama" placeholder="Masukkan Nomor">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kembali</button>
+            <button type="button" class="btn btn-primary disabled" id="saveButton">Bayar</button>
+        </div>
+      </div>
+    </div>
+</div>
+<!-- End -->
   <br>
   <br>
   <div class="container">
@@ -159,5 +224,43 @@
         <button type="button" class="btn btn-primary">Bayar</button>
     </div>
     <!-- End -->
+    <script>
+        function toggleInput() {
+            var inputBank = document.getElementById("inputBank");
+            var checkbox = document.getElementById("radioBRI");
+    
+            // Jika checkbox dicentang, tampilkan input
+            if (checkbox.checked) {
+                inputBank.style.display = "block";
+            } else {
+                // Jika tidak, sembunyikan input
+                inputBank.style.display = "none";
+            }
+        }
+
+        function togleInput() {
+            var inputBank = document.getElementById("inputBank");
+            var checkbox = document.getElementById("radioBTN");
+    
+            // Jika checkbox dicentang, tampilkan input
+            if (checkbox.checked) {
+                inputBank.style.display = "block";
+            } else {
+                // Jika tidak, sembunyikan input
+                inputBank.style.display = "none";
+            }
+        }
+
+        function toggelInput() {
+            var inputEwallet = document.getElementById("inputEwallet");
+            var checkbox = document.getElementById("gopay");
+
+            if (checkbox.checked) {
+                inputEwallet.style.display = "block";
+            } else {
+                inputEwallet.style.display = "none";
+            }
+        }
+    </script>                
 </div>
 @endsection
