60c249ec87bbba6a0bb4aaf080c67638bcb39eea
Andhika Mugni <165865178+Akusehatsekaliya@users.noreply.github.com>
2024-05-15 08:36:58 +0700

    admin keuangan

--- resources/views/admin/keuangan/keuangan.blade.php
+++ resources/views/admin/keuangan/keuangan.blade.php
@@ -2,22 +2,49 @@
 @section('content')
 <div class="content">
     <div class="row">
-        <div class="col-md-12">
-          <div class="card ">
-            <div class="card-header ">
-              <h5 class="card-title">Users Behavior</h5>
-              <p class="card-category">24 Hours performance</p>
-            </div>
-            <div class="card-body ">
-              <canvas id=chartHours width="400" height="100"></canvas>
-            </div>
-            <div class="card-footer ">
-              <div class="stats">
-                <i class="fa fa-history">Updated 3 minutes ago</i>3
-              </div>
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h4 class="card-title">Data Keuangan</h4>
+          </div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table">
+                <thead class=" text-primary">
+                  <th>
+                    Name
+                  </th>
+                  <th>
+                    Country
+                  </th>
+                  <th>
+                    City
+                  </th>
+                  <th class="text-right">
+                    Salary
+                  </th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      Jon Porter
+                    </td>
+                    <td>
+                      Portugal
+                    </td>
+                    <td>
+                      Gloucester
+                    </td>
+                    <td class="text-right">
+                      $98,615
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
       </div>
-</div>
+    </div>
+  </div>
 @endsection
