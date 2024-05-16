d4cd253939a992f9e290203b1729171d2ae3fc4e
Andhika Mugni <165865178+Akusehatsekaliya@users.noreply.github.com>
2024-05-16 08:46:55 +0700

    data masuk ke admin

--- app/Http/Controllers/admin/KeuanganController.php
+++ app/Http/Controllers/admin/KeuanganController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Models\Keuangan;
+use App\Models\Pembayaran;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class KeuanganController extends Controller
 {
     public function keuangan(){
-        return view('admin.keuangan.keuangan');
+        $pembayaran = Keuangan::all();
+        return view('admin.keuangan.keuangan', compact('pembayaran'));
     }
-}   
+}
